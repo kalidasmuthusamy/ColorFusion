a2feca73c4b70b1b75ee19362772e7265ec4bb0c
Dass MK <kalidasm610@gmail.com>
2017-06-26 19:13:04 +0530

    patch fixes

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,13 +2,15 @@ var colors = ["red","#49C549","blue"];
 var text = ["Red","Green","Blue"];
 var lcolor, ltext, rcolor, rtext;
 var lhsElem, rhsElem, image = document.getElementById("sign");
-var count,correct,success,fac=0,yes=0,no=0;
+var count,correct,success,counter,fac=0;
 count = correct = incorrect = 0,i=-1;
 var pattern = [];
-var counter = 61;
+var duration = 61;
 var limit = parseInt((Math.random())*3)+2;
-var yes_button = document.getElementById("yes");
-var no_button = document.getElementById("no");
+var yes_button = document.getElementById("yes-btn");
+var no_button = document.getElementById("no-btn");
+var greenImagePath = "../images/s.png";
+var redImagePath = "../images/n.png";
 
 function genElements()
 {
@@ -29,8 +31,8 @@ function genElements()
     else
       rcolor = lcolor - 1;
   }
-  lhsElem = document.getElementById("lhs");
-  rhsElem = document.getElementById("rhs");
+  lhsElem = document.getElementById("lhs-color");
+  rhsElem = document.getElementById("rhs-color");
   lhsElem.innerHTML = text[ltext];
   rhsElem.innerHTML = text[rtext];
   lhsElem.style.color=colors[lcolor];
@@ -39,39 +41,41 @@ function genElements()
 
 function processAns(elm)
 {
+  function incrementScore(){
+    image.src = greenImagePath;
+    ++correct;
+  }
+
+  function decrementScore(){
+    image.src = redImagePath;
+    --correct;
+  }
+
   ++count;
   var eid = elm.id;
   pattern[++i] = eid;
   if(ltext==rcolor)
   {
-    if(eid=="yes")
+    if(eid=="yes-btn")
     {
-      image.src = "s.png";
-      ++correct;              ++yes;
-      console.log("yes");
+      incrementScore();
     }
 
     else
     {
-      image.src = "n.png";
-      --correct;            ++no;
-      console.log("no");
+      decrementScore();
     }
 
   }
   else
   {
-    if(eid=="no")
+    if(eid=="no-btn")
     {
-      image.src = "s.png";
-      ++correct;          ++no;
-      console.log("no");
+      incrementScore();
     }
     else
     {
-      image.src = "n.png";
-      --correct;                 ++yes;
-      console.log("yes");
+      decrementScore();
     }
   }
   genElements();
@@ -79,6 +83,7 @@ function processAns(elm)
 
 function startActivity()
 {
+  counter = duration;
   setInterval(function () {--counter; document.getElementById("timer-value").innerHTML = "Seconds left : "+counter;}, 1000);
   document.getElementById("start").style.display = "none";
   document.getElementById("timer").style.display = "block";
@@ -91,8 +96,8 @@ function startActivity()
     });
   });
   count = correct = incorrect = 0;
-  myVar = setTimeout(function(){  genResults(); },61000);
-  makeRed = setTimeout(function(){  $("#timer-value").css("color","red"); },51000);
+  myVar = setTimeout(function(){  genResults(); },(duration*1000));
+  makeRed = setTimeout(function(){  $("#timer-value").css("color","red"); },((duration - 10)*1000));
   genElements();
 }
 
@@ -101,11 +106,11 @@ function genResults()
   success = ((correct/count)*100).toFixed(2);
   if(count==0||correct<1)
     success = 0;
-  assignResultsTolocalStorage();
-  window.location.assign("result.html");
+  assignResultsToLocalStorage();
+  window.location.assign("../views/result.html");
 }
 
-function assignResultsTolocalStorage()
+function assignResultsToLocalStorage()
 {
   localStorage.setItem('count',count);
   localStorage.setItem('score',correct);
@@ -139,18 +144,18 @@ function writeResults()
   var accuracy = document.getElementById("accuracy").innerHTML;
   document.getElementById("attempts").innerHTML = attempts + localStorage.getItem('count');
   document.getElementById("score").innerHTML = score + localStorage.getItem('score');
-  document.getElementById("accuracy").innerHTML = accuracy + localStorage.getItem("accuracy"); 
+  document.getElementById("accuracy").innerHTML = accuracy + localStorage.getItem("accuracy");
 }
 
 $("#homeLink").on('click',function(event)
 {
-  if(counter > 0 && counter < 61)
+  if(counter > 0 && counter < duration)
   {
     event.preventDefault();
     var exit = confirm("Game in progress. Want to exit ?");
     if(exit)
     {
-      window.location.assign("index.html");
+      window.location.assign("../index.html");
     }
   }
 });
